c290d3ece28b77186ea70adaaba5221220803133
egoff <egoff@egoff-c02zm2q6md6x.lan>
2021-01-18 10:04:54 -0600

    tinker matirx divide

--- src/org/granitesoft/math/matrix/MatrixField.java
+++ src/org/granitesoft/math/matrix/MatrixField.java
@@ -321,13 +321,11 @@ public class MatrixField<R> implements ExpField<Matrix<R>> {
                 final VectorWrapper<D> row1 = z1.getRow(j);
                 final VectorWrapper<D> row2 = z2.getRow(j);
                 final D div = row1.get(i);
-                if (!f2.isZero(div)) {
-                    for(int k = i + 1; k < size; k++) {
-                        row1.set(k, f2.subtract(row1.get(k), f2.divide(f2.multiply(pivotRow1.get(k), div), pivot)));
-                    }
-                    for(int k = 0; k < size; k++) {
-                        row2.set(k, f2.subtract(row2.get(k), f2.divide(f2.multiply(pivotRow2.get(k), div), pivot)));
-                    }
+                for(int k = i + 1; k < size; k++) {
+                    row1.set(k, f2.subtract(f2.multiply(row1.get(k), pivot), f2.multiply(pivotRow1.get(k), div)));
+                }
+                for(int k = 0; k < size; k++) {
+                    row2.set(k, f2.subtract(f2.multiply(row2.get(k), pivot), f2.multiply(pivotRow2.get(k), div)));
                 }
             }
         }
@@ -340,10 +338,8 @@ public class MatrixField<R> implements ExpField<Matrix<R>> {
                 final VectorWrapper<D> row1 = z1.getRow(j);
                 final VectorWrapper<D> row2 = z2.getRow(j);
                 final D div = row1.get(i);
-                if (!f2.isZero(div)) {
-                    for(int k = 0; k < size; k++) {
-                        row2.set(k, f2.subtract(row2.get(k), f2.divide(f2.multiply(pivotRow2.get(k), div), pivot)));
-                    }
+                for(int k = 0; k < size; k++) {
+                    row2.set(k, f2.subtract(row2.get(k), f2.divide(f2.multiply(pivotRow2.get(k), div), pivot)));
                 }
             }
             final boolean[] inz = isNonZero[i];
